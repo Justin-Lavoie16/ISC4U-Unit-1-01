9427785e9bd573405dc396983c858568a70ea2e9
Justin Lavoie <113055699+Justin-Lavoie16@users.noreply.github.com>
2024-09-20 09:00:34 -0400

    indented java

--- Loglength.java
+++ Loglength.java
@@ -1,11 +1,10 @@
-
 /*
  * much logs a truck can carry if the max weight
-* it can carry is 1100 kg and each log is 20 kg/m
-* *
- * @author  Cristiano Sellitto
+ * it can carry is 1100 kg and each log is 20 kg/m
+ * *
+ * @author  Justin Lavoie
  * @version 1.0
- * @since   2024-02-15
+ * @since   2024-09-20
  */
 
 import java.util.Scanner;
@@ -14,18 +13,18 @@ import java.util.Scanner;
  * This is the program.
  */
 final class checkLogLength {
-            private checkLogLength() {
-                            throw new IllegalStateException("Cannot be instantiated");
-                                }
-                public static void main(final String[] args) {
-                                final Scanner input = new Scanner(System.in);
-                                        System.out.print("Enter log length (m): ");
+    private checkLogLength() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+    public static void main(final String[] args) {
+        final Scanner input = new Scanner(System.in);
+        System.out.print("Enter log length (m): ");
 
-                                                static final float length = Float.parseFloat(input.nextLine());
-                                                        static final float maxLogsCarried = 1100 / (20 * length);
+        static final float length = Float.parseFloat(input.nextLine());
+        static final float maxLogsCarried = 1100 / (20 * length);
 
-                                                                System.out.println("A truck can carry " + maxLogsCarried + " " + length + " meter long logs.");
+        System.out.println("A truck can carry " + maxLogsCarried + " " + length + " meter long logs.");
 
-                                                                        System.out.println("\nDone.");
-                                                                            }
+        System.out.println("\nDone.");
+    }
 }
